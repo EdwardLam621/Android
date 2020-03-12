7b36595c2bbaa97fcdf9eee482f65a9b36ddc6e8
Edward <lamhoutenged@seattleu.edu>
2020-03-11 23:57:33 -0700

    Hook up button in MainActivity

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button = (Button) findViewById(R.id.button2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                // to be implemented
+            }
+        });
     }
+
+
+
+
 }
