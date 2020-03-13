7dd4db34158b784b2e773654ff9f6f9ef54f40d7
Edward <lamhoutenged@seattleu.edu>
2020-03-13 15:05:18 -0700

    Added GlobalModel

---
+++ app/src/main/java/com/example/android/GlobalModel.java
@@ -0,0 +1,11 @@
+package com.example.android;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+
+public class GlobalModel {
+
+    // The app context
+    @SuppressLint("StaticFieldLeak")
+    public static Context MainActivityContext;
+}
